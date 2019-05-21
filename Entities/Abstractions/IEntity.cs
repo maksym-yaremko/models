f4282d5630809eb3ba8008e7618662b4cc9ec21c
Maksym Yaremko <maksym.yaremko@gmail.com>
2019-05-21 17:40:53 +0300

    1

---
+++ Entities/Abstractions/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransIT.DAL.Models.Entities.Abstractions
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+        int? CreateId { get; set; }
+        int? ModId { get; set; }
+    }
+}
